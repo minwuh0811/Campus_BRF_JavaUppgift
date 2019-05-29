ebfaeae867d15e2ce3f5a0fdeacab364c64a46fa
minwuh081@gmail.com <w860314m>
2019-05-29 11:40:53 +0200

    Variable default 0 for integer och null for string

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
-import javax.swing.*;
-
 public class Main {
-    public static String s = "Hello Wold";
-    public static int i = 5;
+    public static String s;
+    public static int i;
 
     public static void main(String[] args) {
-        int j = 10;
-        if (j > i) {
+        int j=i; i=3;
+        if (j >= i) {
             System.out.println("Hej");
         } else if(j<i) {
-            System.out.printf(s);
+            System.out.println(s);
         } else{
-            System.out.printf("I did't know you!!");
+            System.out.println("I did't know you!!");
         }
     }
 }
