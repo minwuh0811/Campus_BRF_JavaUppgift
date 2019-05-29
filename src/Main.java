63931594fe24d7a2c35e1c098f52b0c2863ce950
minwuh081@gmail.com <w860314m>
2019-05-29 11:09:08 +0200

    Variable local/global

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ import javax.swing.*;
 
 public class Main {
     public static String s = "Hello Wold";
+    public static int i = 5;
 
     public static void main(String[] args) {
-        System.out.println(s);
+        int i = 10;
+        System.out.println("Hello World" + i); //local > global
         JOptionPane.showMessageDialog(null,s);
     }
 }
