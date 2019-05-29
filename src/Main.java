d26cb1b4cd68bde4ec64e5f493732541758f1bbf
minwuh081@gmail.com <w860314m>
2019-05-29 10:56:23 +0200

    Hello world

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+
 public class Main {
+    public static String s = "Hello Wold";
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println(s);
+        JOptionPane.showMessageDialog(null,s);
     }
 }
